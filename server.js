551db77b13c4b3d882a4373859febac1ee65e2ba
Allen Vestal <allen@godaddy.com>
2017-04-14 17:21:24 -0700

    updated script files
    updated:
    index file script tag
    updated port for dockerfile
    updated port for server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // set up ========================
-    var express  = require('express');
-    var app      = express();                               // create our app w/ express
-    var morgan = require('morgan');             // log requests to the console (express4)
+var express = require('express');
+var app = express(); // create our app w/ express
+var morgan = require('morgan'); // log requests to the console (express4)
 
 // configuration
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(morgan('dev'));                                         // log every request to the console
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
 
 // routes
 
 // application
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 // listen (start app with node server.js) ======================================
-    app.listen(8080);
-    console.log("App listening on port 8080");
+app.listen(8080);
+console.log("App listening on port 8080");
