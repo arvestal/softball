5d7d9c91b071f913fb1cbd19ce22496df8039aea
Allen Vestal <allen@godaddy.com>
2017-10-27 16:01:33 -0700

    Added morgan logging

---
+++ public/lib/logger.js
@@ -0,0 +1,40 @@
+const winston = require('winston');
+const slack = require('winston-slack-hook');
+const tsFormat = () => (new Date()).toLocaleTimeString();
+
+const log = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      level: 'debug',
+      timestamp: tsFormat,
+      handleExceptions: true,
+      json: false,
+      colorize: true
+    }),
+    new slack({
+      hookUrl: 'https://hooks.slack.com/services/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',
+      username: '@jeeves',
+      channel: '#softball',
+      formatter: (options) => {
+        let message = options.message;
+        const level = options.level;
+        message = `${tsFormat()} ${level} ${message}`;
+        return message;
+      },
+      colors: {
+        warn: 'warning',
+        error: 'danger',
+        info: 'good',
+        debug: '#bbddff'
+      }
+    })
+  ]
+});
+
+module.exports = log;
+
+module.exports.stream = {
+  write: (message, encoding) => {
+    log.info(message);
+  }
+};
--- server.js
+++ server.js
@@ -3,19 +3,23 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const routes = require('./public/lib/routes');
+const log = require('./public/lib/logger');
 
 // configuration
 
 app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
+app.use(morgan({
+  // skip: (req, res) => {
+  //   return res.statusCode < 400;
+  // },
+  stream: log.stream
+}));
 
 // routes
 app.use('/api', routes);
-// application
-// app.get('*', function(req, res) {
-//     res.sendFile(__dirname + '/public/index.html');
-// });
+
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
-console.log('App listening on port 8080');
+log.info('App listening on port 8080');
