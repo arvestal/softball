70fc7911ea565e393a5c5adb9ed544e4b5f405b6
Allen Vestal <allen@godaddy.com>
2018-06-19 00:12:42 -0700

    updates 2018 summer stats

--- public/lib/routes.js
+++ public/lib/routes.js
@@ -60,7 +60,7 @@ router.get('/summer18', (request, response) => {
 router.get('/standings', (request, response) => {
   const results = {
     coed: {wins: 14, losses: 1, ties: 1},
-    postseason: {wins: 12, losses: 13},
+    postseason: {wins: 13, losses: 13},
     summer18: {wins: 4, losses: 0, ties: 0},
     spring18: {wins: 8, losses: 6, ties: 0},
     winter18: {wins: 9, losses: 1, ties: 0},
