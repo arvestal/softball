bae7075dde2187ca3214bc4e9c48fae08475da77
Allen Vestal <allen@godaddy.com>
2018-09-13 14:16:23 -0700

    setup fall season 2018 and updated stats for week 1 Thirsty Camels

--- public/lib/routes.js
+++ public/lib/routes.js
@@ -57,10 +57,20 @@ router.get('/summer18', (request, response) => {
   });
 });
 
+router.get('/fall18', (request, response) => {
+  const csvFile = __dirname + "/18fall.csv";
+  stats.convertToJson(csvFile)
+  .then((data) => {
+      console.log(data);
+      response.json(data);
+  });
+});
+
 router.get('/standings', (request, response) => {
   const results = {
     coed: {wins: 14, losses: 1, ties: 1},
-    postseason: {wins: 13, losses: 13},
+    postseason: {wins: 15, losses: 13},
+    fall18: {wins:2, losses:0, ties:0},
     summer18: {wins: 8, losses: 3, ties: 1},
     spring18: {wins: 8, losses: 6, ties: 0},
     winter18: {wins: 9, losses: 1, ties: 0},
