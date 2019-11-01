ab11a23c3952a101494ece191dbd6b10be07016e
Allen Vestal <arvestal@gmail.com>
2019-11-01 15:12:03 -0700

    update end of season stats for fall 2019

--- public/lib/routes.js
+++ public/lib/routes.js
@@ -14,15 +14,15 @@ router.get("/stats", (request, response) => {
 router.get("/standings", (request, response) => {
   const results = {
     coed: { wins: 14, losses: 1, ties: 1 },
-    postseason: { wins: 16, losses: 15 },
-    /*winter19: { wins: 0, losses: 0, ties: 0 },*/
+    postseason: { wins: 18, losses: 16 },
+    /*winter20: { wins: 0, losses: 0, ties: 0 },*/
     fall19: { wins: 12, losses: 2, ties: 0 },
     spring19: { wins: 10, losses: 3, ties: 1 },
     fall18: { wins: 5, losses: 5, ties: 0 },
     summer18: { wins: 8, losses: 3, ties: 1 },
     spring18: { wins: 8, losses: 6, ties: 0 },
     winter18: { wins: 9, losses: 1, ties: 0 },
-    fall17: { wins: 10, losses: 4, ties: 0 },
+    fall17: { wins: 11, losses: 3, ties: 0 },
     summer17: { wins: 8, losses: 6, ties: 0 },
     spring17: { wins: 7, losses: 8, ties: 1 },
     winter17: { wins: 9, losses: 5, ties: 0 },
@@ -34,7 +34,7 @@ router.get("/standings", (request, response) => {
     summer15: { wins: 10, losses: 4, ties: 0 },
     spring15: { wins: 3, losses: 11, ties: 0 },
     winter15: { wins: 7, losses: 7, ties: 0 },
-    fall14: { wins: 7, losses: 5, ties: 2 },
+    fall14: { wins: 8, losses: 4, ties: 2 },
     summer14: { wins: 8, losses: 5, ties: 1 }
   };
 
